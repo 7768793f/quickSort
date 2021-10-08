b1a7550e29ea33624ad23f3075cfd67456938b68
7768793f <92097486+7768793f@users.noreply.github.com>
2021-10-08 09:37:17 +0300

    Add files via upload
    program correctly identifies all of the numbers that need to be swapped, but does not swap them

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 import random as r
 
 unsorted = [r.randint(0,50) for i in range(0,15)]   #Target list
-pivot = unsorted[r.randint(0,len(unsorted)-1)]      #pivot for splitting list
-sList = unsorted[pivot:]                            # list of smaller values (left of pivot)
-bList = unsorted[:pivot]                            # list of bigger values (right of pivot)
+pivotIndex = len(unsorted) // 2
+pivotVal = unsorted[pivotIndex]
+#pivot = unsorted[r.randint(0,len(unsorted)-1)]      #pivot for splitting list
+sList = unsorted[:pivotIndex]                            # list of smaller values (left of pivot)
+bList = unsorted[pivotIndex:]                            # list of bigger values (right of pivot)
 
 print(unsorted)
-print("PIVOT IS:", pivot)
+print(sList)
+print(bList)
+print("PIVOT IS:", pivotVal)
 
 sIndex = 0      #declaring variables that need to be global
-small = 0
+smallToBig = "null"
 bIndex = 0
-big = 0
+bigToSmall = "null"
 sListDone = False
 bListDone = False
+i = 0
+y = 0
 
 while sListDone != True or bListDone != True:
-    sList = unsorted[pivot:]
-    bList = unsorted[:pivot]
-    for i in range (0, len(sList)):
-        if sList[i] > pivot:
-            small = sList[i]
-            sIndex = i
-            print("s", small)
-            break
-        else:
-            sListDone = True
-
-    for y in range (0, len(bList)):
-        if bList[y] < pivot:
-            big = bList[y]
-            bIndex = y
-            print("b", big)
-            break
-        else:
-            bListDone = True
-    unsorted[sIndex] = big
-    unsorted[bIndex+len(sList)] = small
-
+    if smallToBig == "null":
+        for i in range (i, len(sList)):
+            if sList[i] > pivotVal:
+                smallToBig = sList[i]
+                sIndex = i
+                print("s", smallToBig)
+            elif i == len(sList):
+                sListDone = True
+    if bigToSmall == "null":
+        for y in range (y, len(bList)):
+            if bList[y] < pivotVal:
+                bigToSmall = bList[y]
+                bIndex = y
+                print("b", bigToSmall)
+            elif y == len(bList):
+                bListDone = True
+    unsorted[sIndex] = bigToSmall                  #swapping numbers
+    unsorted[bIndex+len(sList)] = smallToBig
+    #print(unsorted)
     #print(big, "Is the big value that has been swapped with", small)
